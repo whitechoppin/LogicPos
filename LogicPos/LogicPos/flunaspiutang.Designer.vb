a66d09e62a7a4d3e9ea6d5cecf845383e05e0618
Alex Pratama Wibowo <alexanderwibowo.aw@gmail.com>
2019-12-06 13:21:56 +0800

    Lunas Piutang

--- LogicPos/LogicPos/flunaspiutang.Designer.vb
+++ LogicPos/LogicPos/flunaspiutang.Designer.vb
@@ -22,8 +22,16 @@ Partial Class flunaspiutang
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'flunaspiutang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "flunaspiutang"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Name = "flunaspiutang"
+        Me.Text = " "
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
