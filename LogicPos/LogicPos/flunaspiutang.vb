aaf84f98bed731b7d3c5fa04e574035dc5712096
Alex Pratama Wibowo <alexanderwibowo.aw@gmail.com>
2019-12-13 17:04:54 +0800

    Lunas Utang 1

--- LogicPos/LogicPos/flunaspiutang.vb
+++ LogicPos/LogicPos/flunaspiutang.vb
@@ -1,4 +1,5 @@
-﻿Imports DevExpress.Utils
+﻿Imports System.Data.Odbc
+Imports DevExpress.Utils
 
 Public Class flunaspiutang
     Public tabel1, tabel2 As DataTable
@@ -11,7 +12,7 @@ Public Class flunaspiutang
             'agar muncul footer untuk sum/avg/count
             .OptionsView.ShowFooter = True
             'buat sum harga
-            .Columns("subtotal").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "subtotal", "{0:n0}")
+            .Columns("Bayar").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "Bayar", "{0:n0}")
         End With
     End Sub
 
@@ -21,95 +22,91 @@ Public Class flunaspiutang
         With tabel1
             .Columns.Add("kode_penjualan")
             .Columns.Add("kode_costumer")
-            .Columns.Add("nama_barang")
-            .Columns.Add("banyak", GetType(Double))
-            .Columns.Add("satuan_barang")
-            .Columns.Add("jenis_barang")
-            .Columns.Add("harga_satuan", GetType(Double))
-            .Columns.Add("diskon_persen", GetType(Double))
-            .Columns.Add("diskon_nominal", GetType(Double))
-            .Columns.Add("harga_diskon", GetType(Double))
-            .Columns.Add("subtotal", GetType(Double))
-            .Columns.Add("laba", GetType(Double))
-            .Columns.Add("modal_barang", GetType(Double))
+            .Columns.Add("kode_gudang")
+            .Columns.Add("kode_user")
+            .Columns.Add("tgl_penjualan")
+            .Columns.Add("tgl_jatuhtempo_penjualan")
+            .Columns.Add("diskon_penjualan", GetType(Double))
+            .Columns.Add("pajak_penjualan", GetType(Double))
+            .Columns.Add("ongkir_penjualan", GetType(Double))
+            .Columns.Add("total_penjualan", GetType(Double))
+            .Columns.Add("bayar_penjualan", GetType(Double))
+            .Columns.Add("sisa_penjualan", GetType(Double))
         End With
 
         GridControl1.DataSource = tabel1
 
-        GridColumn1.FieldName = "kode_barang"
-        GridColumn1.Caption = "Kode Barang"
+        GridColumn1.FieldName = "kode_penjualan"
+        GridColumn1.Caption = "Kode Penjualan"
         GridColumn1.Width = 20
 
-        GridColumn2.FieldName = "kode_stok"
-        GridColumn2.Caption = "Kode Stok"
+        GridColumn2.FieldName = "kode_costumer"
+        GridColumn2.Caption = "Kode Costumer"
         GridColumn2.Width = 20
 
-        GridColumn3.FieldName = "nama_barang"
-        GridColumn3.Caption = "Nama Barang"
-        GridColumn3.Width = 70
+        GridColumn3.FieldName = "kode_gudang"
+        GridColumn3.Caption = "Kode Gudang"
+        GridColumn3.Width = 20
 
-        GridColumn4.FieldName = "banyak"
-        GridColumn4.Caption = "banyak"
-        GridColumn4.DisplayFormat.FormatType = FormatType.Numeric
-        GridColumn4.DisplayFormat.FormatString = "{0:n0}"
-        GridColumn4.Width = 5
+        GridColumn4.FieldName = "kode_user"
+        GridColumn4.Caption = "Kode User"
+        GridColumn4.Width = 20
 
-        GridColumn5.FieldName = "satuan_barang"
-        GridColumn5.Caption = "Satuan Barang"
-        GridColumn5.Width = 10
+        GridColumn5.FieldName = "tgl_penjualan"
+        GridColumn5.Caption = "Tgl Penjualan"
+        GridColumn5.Width = 20
 
-        GridColumn6.FieldName = "jenis_barang"
-        GridColumn6.Caption = "Jenis Barang"
-        GridColumn6.Width = 10
+        GridColumn6.FieldName = "tgl_jatuhtempo_penjualan"
+        GridColumn6.Caption = "Tgl Jatuh Tempo"
+        GridColumn6.Width = 20
 
-        GridColumn7.FieldName = "harga_satuan"
-        GridColumn7.Caption = "Harga Satuan"
+        GridColumn7.FieldName = "diskon_penjualan"
+        GridColumn7.Caption = "Diskon"
         GridColumn7.DisplayFormat.FormatType = FormatType.Numeric
         GridColumn7.DisplayFormat.FormatString = "{0:n0}"
         GridColumn7.Width = 20
 
-        GridColumn8.FieldName = "diskon_persen"
-        GridColumn8.Caption = "Diskon %"
+        GridColumn8.FieldName = "pajak_penjualan"
+        GridColumn8.Caption = "Pajak"
         GridColumn8.DisplayFormat.FormatType = FormatType.Numeric
         GridColumn8.DisplayFormat.FormatString = "{0:n0}"
         GridColumn8.Width = 20
 
-        GridColumn9.FieldName = "diskon_nominal"
-        GridColumn9.Caption = "Diskon Nominal"
+        GridColumn9.FieldName = "ongkir_penjualan"
+        GridColumn9.Caption = "Ongkir"
         GridColumn9.DisplayFormat.FormatType = FormatType.Numeric
         GridColumn9.DisplayFormat.FormatString = "{0:n0}"
         GridColumn9.Width = 30
 
-        GridColumn10.FieldName = "harga_diskon"
-        GridColumn10.Caption = "Harga Diskon"
+        GridColumn10.FieldName = "total_penjualan"
+        GridColumn10.Caption = "Total"
         GridColumn10.DisplayFormat.FormatType = FormatType.Numeric
         GridColumn10.DisplayFormat.FormatString = "{0:n0}"
         GridColumn10.Width = 30
 
-        GridColumn11.FieldName = "subtotal"
-        GridColumn11.Caption = "Subtotal"
+        GridColumn11.FieldName = "bayar_penjualan"
+        GridColumn11.Caption = "Bayar"
         GridColumn11.DisplayFormat.FormatType = FormatType.Numeric
         GridColumn11.DisplayFormat.FormatString = "{0:n0}"
         GridColumn11.Width = 30
 
-        GridColumn12.FieldName = "laba"
-        GridColumn12.Caption = "Laba"
+        GridColumn12.FieldName = "sisa_penjualan"
+        GridColumn12.Caption = "Sisa"
         GridColumn12.DisplayFormat.FormatType = FormatType.Numeric
         GridColumn12.DisplayFormat.FormatString = "{0:n0}"
-        GridColumn12.Width = 20
-        GridColumn12.Visible = False
+        GridColumn12.Width = 30
 
     End Sub
     Sub tabel_lunas()
         tabel2 = New DataTable
 
         With tabel2
-            .Columns.Add("kode_barang")
-            .Columns.Add("kode_stok")
-            .Columns.Add("nama_barang")
-            .Columns.Add("banyak", GetType(Double))
-            .Columns.Add("satuan_barang")
-            .Columns.Add("jenis_barang")
+            .Columns.Add("kode_lunas")
+            .Columns.Add("kode_penjualan")
+            .Columns.Add("tanggal_transaksi")
+            .Columns.Add("kode_user")
+            .Columns.Add("kode_kas")
+            .Columns.Add("bayar_kas", GetType(Double))
 
         End With
 
@@ -136,6 +133,25 @@ Public Class flunaspiutang
         GridColumn18.FieldName = "satuan_barang"
         GridColumn18.Caption = "Satuan Barang"
         GridColumn18.Width = 10
+
+    End Sub
+
+    Sub reload_tabel()
+        GridControl1.RefreshDataSource()
+        txtnonota.Clear()
+        txtcustomer.Clear()
+    End Sub
+    Sub loadingpenjualan(lihat As String)
+        Call tabel_utama()
+        Call tabel_lunas()
+        sql = "SELECT * FROM tb_penjualan WHERE kode_penjualan ='" & lihat & "'"
+        cmmd = New OdbcCommand(sql, cnn)
+        dr = cmmd.ExecuteReader()
+        While dr.Read
+            tabel1.Rows.Add(dr("kode_penjualan"), dr("kode_pelanggan"), dr("nama_barang"), dr("qty"), dr("satuan_barang"), dr("jenis_barang"), Val(dr("harga_jual")), Val(dr("diskon")), Val(dr("harga_jual")) * Val(dr("diskon")) / 100, dr("harga_jual") - (Val(dr("harga_jual")) * Val(dr("diskon")) / 100), Val(dr("subtotal")), Val(dr("keuntungan")), Val(dr("modal")))
+            'tabel.Rows.Add(dr("kode_stok"), dr("kode_barang"), dr("nama_barang"), dr("qty"), dr("satuan_barang"), dr("jenis_barang"), Val(dr("harga_jual")), dr("diskon"), 0, dr("harga_diskon"), dr("subtotal"), 0, 0)
+            GridControl1.RefreshDataSource()
+        End While
 
     End Sub
 
--- LogicPos/LogicPos/flunasutang.vb
+++ LogicPos/LogicPos/flunasutang.vb
@@ -1,3 +1,13 @@
 ﻿Public Class flunasutang
+    Private Sub flunasutang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.MdiParent = fmenu
+        Call koneksii()
 
+        With GridView2
+            'agar muncul footer untuk sum/avg/count
+            .OptionsView.ShowFooter = True
+            'buat sum harga
+            .Columns("Bayar").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "Bayar", "{0:n0}")
+        End With
+    End Sub
 End Class
