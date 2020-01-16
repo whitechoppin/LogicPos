613615ce385df72f280aa295ea0c2af307af2924
Wendy Thung <wendy.thung93@gmail.com>
2020-01-16 18:53:33 +0800

    Add - Flogin

---
+++ LogicPos/LogicPos/flogin.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class flogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(flogin))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.btnlogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(11, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'txtusername
+        '
+        Me.txtusername.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtusername.Location = New System.Drawing.Point(94, 10)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(119, 26)
+        Me.txtusername.TabIndex = 2
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpassword.Location = New System.Drawing.Point(94, 42)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(119, 26)
+        Me.txtpassword.TabIndex = 3
+        '
+        'btnlogin
+        '
+        Me.btnlogin.BackgroundImage = CType(resources.GetObject("btnlogin.BackgroundImage"), System.Drawing.Image)
+        Me.btnlogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnlogin.Location = New System.Drawing.Point(219, 10)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(72, 58)
+        Me.btnlogin.TabIndex = 4
+        Me.btnlogin.UseVisualStyleBackColor = True
+        '
+        'flogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(303, 85)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "flogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtusername As TextBox
+    Friend WithEvents txtpassword As TextBox
+    Friend WithEvents btnlogin As Button
+End Class
---
+++ LogicPos/LogicPos/flogin.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Data.Odbc
+Public Class flogin
+    Sub login()
+        Call koneksii()
+        sql = "SELECT * FROM tb_user WHERE kode_user = '" + txtusername.Text + "' AND password_user= '" + txtpassword.Text + "'"
+        cmmd = New OdbcCommand(sql, cnn)
+        dr = cmmd.ExecuteReader()
+        dr.Read()
+
+        If dr.HasRows = 0 Then
+            MsgBox("Username atau Password ada yang salah !", MsgBoxStyle.Exclamation, "Error Login")
+        Else
+            MsgBox("Selamat Datang " & txtusername.Text & " ! ", MsgBoxStyle.Information, "Successfull Login")
+            'Call offform()
+            'sql = "SELECT * FROM tb_detailuser WHERE username = '" + txtusername.Text + "'"
+            'cmmd = New OdbcCommand(sql, cnn)
+            'dr = cmmd.ExecuteReader()
+            'While dr.Read
+            '    Dim a As Integer
+            '    a = CInt(dr("idhead"))
+            '    'MsgBox(dr("idhead").ToString)
+            '    fmenu.MenuUtama.Items.Item(a).Visible = True
+            'End While
+
+            'sql = "SELECT * FROM tb_detailuser WHERE username = '" + txtusername.Text + "'"
+            'cmmd = New OdbcCommand(sql, cnn)
+            'dr = cmmd.ExecuteReader()
+            'While dr.Read
+            '    sql = "SELECT * FROM tb_detailuser WHERE username = '" + txtusername.Text + "' and idhead= 0 "
+            '    cmmd = New OdbcCommand(sql, cnn)
+            '    dr = cmmd.ExecuteReader()
+            '    While dr.Read
+            '        Dim b As Integer
+            '        b = CInt(dr("idchild"))
+            '        If b = 9 Then
+            '            On Error Resume Next
+            '        Else
+            '            'MsgBox(dr("idchild").ToString)
+            '            fmenu.mastermenu.DropDownItems.Item(b).Visible = True
+            '            On Error Resume Next
+            '        End If
+            '    End While
+
+
+            '    sql = "SELECT * FROM tb_detailuser WHERE username = '" + txtusername.Text + "' and idhead= 2 "
+            '    cmmd = New OdbcCommand(sql, cnn)
+            '    dr = cmmd.ExecuteReader()
+            '    While dr.Read
+            '        Dim c As Integer
+            '        c = dr("idchild")
+            '        If c = 9 Then
+            '            On Error Resume Next
+            '        Else
+            '            fmenu.AdministrasiMenu.DropDownItems.Item(c).Visible = True
+            '            On Error Resume Next
+            '        End If
+            '    End While
+
+            '    sql = "SELECT * FROM tb_detailuser WHERE username = '" + txtusername.Text + "' and idhead= 3 "
+            '    cmmd = New OdbcCommand(sql, cnn)
+            '    dr = cmmd.ExecuteReader()
+            '    While dr.Read
+            '        Dim d As Integer
+            '        d = dr("idchild")
+            '        If d = 9 Then
+            '            On Error Resume Next
+            '        Else
+            '            fmenu.transaksimenu.DropDownItems.Item(d).Visible = True
+            '            On Error Resume Next
+            '        End If
+            '    End While
+
+            '    sql = "SELECT * FROM tb_detailuser WHERE username = '" + txtusername.Text + "' and idhead=4 "
+            '    cmmd = New OdbcCommand(sql, cnn)
+            '    dr = cmmd.ExecuteReader()
+
+            '    While dr.Read
+            '        Dim e As Integer
+            '        e = dr("idchild")
+            '        If e = 9 Then
+            '            On Error Resume Next
+            '        Else
+            '            fmenu.laporanmenu.DropDownItems.Item(e).Visible = True
+            '            On Error Resume Next
+            '        End If
+            '    End While
+            'End While
+
+            fmenu.statususer.Text = txtusername.Text.ToUpper
+            fmenu.Show()
+        End If
+    End Sub
+
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        If txtusername.Text = "" Then
+            MessageBox.Show("User masih kosong", "username", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+            txtusername.Focus()
+            Exit Sub
+        End If
+        If txtpassword.Text = "" Then
+            MessageBox.Show("Password masih kosong", "password", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+            txtpassword.Focus()
+        Else
+        End If
+        Call login()
+    End Sub
+
+
+    Private Sub flogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtpassword.UseSystemPasswordChar = False
+        txtpassword.PasswordChar = "•"
+    End Sub
+End Class
