3193c0e549659f07dddaecbd32d943862c3c75fd
Wendy Thung <wendy.thung93@gmail.com>
2020-02-12 13:35:14 +0800

    cek new report 12.02 - laporan retur jual

--- LogicPos/LogicPos/flaporanreturjual.vb
+++ LogicPos/LogicPos/flaporanreturjual.vb
@@ -1,3 +1,212 @@
-﻿Public Class flaporanreturjual
+﻿Imports System.Data.Odbc
+Imports System.Globalization
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+Imports DevExpress.XtraGrid.Columns
+Public Class flaporanreturjual
+    Public isi As String
+    Public isi2 As String
+    Private Sub flaporanpenjualan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.MdiParent = fmenu
+        Call koneksii()
 
+        DateTimePicker1.MaxDate = Now
+        DateTimePicker2.MaxDate = Now
+        Call grid()
+
+        With GridView1
+            .OptionsView.ShowFooter = True 'agar muncul footer untuk sum/avg/count
+            'buat sum harga
+            .Columns("subtotal").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "subtotal", "{0:n0}")
+            .Columns("keuntungan").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "keuntungan", "{0:n0}")
+
+        End With
+    End Sub
+    Sub grid()
+        GridColumn1.Caption = "No.Nota"
+        GridColumn1.FieldName = "kode_penjualan"
+
+        GridColumn2.Caption = "Pelangan"
+        GridColumn2.FieldName = "nama_pelanggan"
+        GridColumn2.Visible = False
+
+        GridColumn3.Caption = "Tanggal Retur Penjualan"
+        GridColumn3.FieldName = "tgl_returjual"
+        GridColumn3.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn3.DisplayFormat.FormatString = "dd/MM/yyy"
+
+        GridColumn4.Caption = "Item"
+        GridColumn4.FieldName = "nama_barang"
+
+        GridColumn5.Caption = "Banyak"
+        GridColumn5.FieldName = "qty"
+
+        GridColumn6.Caption = "Satuan"
+        GridColumn6.FieldName = "satuan_barang"
+
+        GridColumn7.Caption = "Harga Jual"
+        GridColumn7.FieldName = "harga_jual"
+        GridColumn7.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn7.DisplayFormat.FormatString = "##,##0"
+
+        GridColumn8.Caption = "Subtotal"
+        GridColumn8.FieldName = "subtotal"
+        GridColumn8.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn8.DisplayFormat.FormatString = "##,##0"
+
+        GridColumn9.Caption = "Laba"
+        GridColumn9.FieldName = "keuntungan"
+        GridColumn9.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn9.DisplayFormat.FormatString = "##,##0"
+        GridColumn9.Visible = False
+
+        GridColumn10.Visible = False
+        GridColumn11.Caption = "Kasir"
+        GridColumn11.FieldName = "kode_user"
+        GridColumn12.Caption = "Metode Bayar"
+        GridColumn12.FieldName = "metode_pembayaran"
+        GridColumn12.Visible = False
+        GridControl1.Visible = True
+    End Sub
+    Sub tabel()
+        Call koneksii()
+        Using cnn As New OdbcConnection(strConn)
+            If DateTimePicker1.Value.Equals(DateTimePicker2.Value) Then
+                sql = "SELECT * FROM tb_retur_penjualan_detail JOIN tb_retur_penjualan ON tb_retur_penjualan.kode_retur=tb_retur_penjualan_detail.kode_retur WHERE DATE(tgl_returjual) = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "'"
+            Else
+                sql = "Select * FROM tb_retur_penjualan_detail JOIN tb_retur_penjualan On tb_retur_penjualan.kode_retur=tb_retur_penjualan_detail.kode_retur  WHERE tgl_returjual BETWEEN '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "' AND '" & Format(DateTimePicker2.Value, "yyyy-MM-dd") & "'"
+            End If
+            da = New OdbcDataAdapter(sql, cnn)
+            ds = New DataSet
+            da.Fill(ds)
+            GridControl1.DataSource = Nothing
+            GridControl1.DataSource = ds.Tables(0)
+            Call grid()
+        End Using
+    End Sub
+
+
+    Private Sub btnrekap_Click(sender As Object, e As EventArgs) Handles btnrekap.Click
+        Dim rptrekap As ReportDocument
+        Dim awalPFDs As ParameterFieldDefinitions
+        Dim awalPFD As ParameterFieldDefinition
+        Dim awalPVs As New ParameterValues
+        Dim awalPDV As New ParameterDiscreteValue
+
+        Dim akhirPFDs As ParameterFieldDefinitions
+        Dim akhirPFD As ParameterFieldDefinition
+        Dim akhirPVs As New ParameterValues
+        Dim akhirPDV As New ParameterDiscreteValue
+
+        If DateTimePicker1.Value.Equals(DateTimePicker2.Value) Then
+            sql = "SELECT * FROM tb_retur_penjualan WHERE DATE(tgl_returjual) = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "'"
+        Else
+            sql = "Select * FROM tb_retur_penjualan WHERE tgl_returjual BETWEEN '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "' AND '" & Format(DateTimePicker2.Value, "yyyy-MM-dd") & "'"
+        End If
+
+        cmmd = New OdbcCommand(sql, cnn)
+        dr = cmmd.ExecuteReader
+
+        If dr.HasRows Then
+            rptrekap = New rptrekapreturjual
+
+            awalPDV.Value = Format(DateTimePicker1.Value, "yyyy-MM-dd")
+            awalPFDs = rptrekap.DataDefinition.ParameterFields
+            awalPFD = awalPFDs.Item("tglawal") 'tanggal merupakan nama parameter
+            awalPVs.Clear()
+            awalPVs.Add(awalPDV)
+            awalPFD.ApplyCurrentValues(awalPVs)
+
+            akhirPDV.Value = Format(DateTimePicker2.Value, "yyyy-MM-dd")
+            akhirPFDs = rptrekap.DataDefinition.ParameterFields
+            akhirPFD = akhirPFDs.Item("tglakhir") 'tanggal merupakan nama parameter
+            akhirPVs.Clear()
+            akhirPVs.Add(akhirPDV)
+            akhirPFD.ApplyCurrentValues(akhirPVs)
+
+            flappenjualan.CrystalReportViewer1.ReportSource = rptrekap
+            flappenjualan.ShowDialog()
+            flappenjualan.WindowState = FormWindowState.Maximized
+        Else
+            MsgBox("Data pada tanggal tersebut tidak tersedia", MsgBoxStyle.Information, "Pemberitahuan")
+        End If
+    End Sub
+
+    Private Sub btntabel_Click(sender As Object, e As EventArgs) Handles btntabel.Click
+        Call tabel()
+    End Sub
+    Sub ExportToExcel()
+
+        Dim filename As String = InputBox("Nama File", "Input Nama file ")
+        Dim pathdata As String = "C:\ExportLogicPos"
+        Dim yourpath As String = "C:\ExportLogicPos\" + filename + ".xls"
+
+        If filename <> "" Then
+            If (Not System.IO.Directory.Exists(pathdata)) Then
+                System.IO.Directory.CreateDirectory(pathdata)
+            End If
+
+            GridView1.ExportToXls(yourpath)
+            MsgBox("Data tersimpan di " + yourpath, MsgBoxStyle.Information, "Success")
+            ' Do something
+        ElseIf DialogResult.Cancel Then
+            MsgBox("You've canceled")
+        End If
+
+    End Sub
+    Private Sub btnexcel_Click(sender As Object, e As EventArgs) Handles btnexcel.Click
+        If GridView1.DataRowCount > 0 Then
+            ExportToExcel()
+        Else
+            MsgBox("Export Gagal, Rekap Tabel terlebih dahulu  !", MsgBoxStyle.Information, "Gagal")
+        End If
+
+
+    End Sub
+
+    Private Sub btnfaktur_Click(sender As Object, e As EventArgs) Handles btnfaktur.Click
+        Dim rptrekap As ReportDocument
+        Dim awalPFDs As ParameterFieldDefinitions
+        Dim awalPFD As ParameterFieldDefinition
+        Dim awalPVs As New ParameterValues
+        Dim awalPDV As New ParameterDiscreteValue
+
+        Dim akhirPFDs As ParameterFieldDefinitions
+        Dim akhirPFD As ParameterFieldDefinition
+        Dim akhirPVs As New ParameterValues
+        Dim akhirPDV As New ParameterDiscreteValue
+
+        If DateTimePicker1.Value.Equals(DateTimePicker2.Value) Then
+            sql = "SELECT * FROM tb_retur_penjualan WHERE DATE(tgl_returjual) = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "'"
+        Else
+            sql = "Select * FROM tb_retur_penjualan WHERE tgl_returjual BETWEEN '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "' AND '" & Format(DateTimePicker2.Value, "yyyy-MM-dd") & "'"
+        End If
+
+        cmmd = New OdbcCommand(sql, cnn)
+        dr = cmmd.ExecuteReader
+
+        If dr.HasRows Then
+            rptrekap = New rptperfakturreturjual
+
+            awalPDV.Value = Format(DateTimePicker1.Value, "yyyy-MM-dd")
+            awalPFDs = rptrekap.DataDefinition.ParameterFields
+            awalPFD = awalPFDs.Item("tglawal") 'tanggal merupakan nama parameter
+            awalPVs.Clear()
+            awalPVs.Add(awalPDV)
+            awalPFD.ApplyCurrentValues(awalPVs)
+
+            akhirPDV.Value = Format(DateTimePicker2.Value, "yyyy-MM-dd")
+            akhirPFDs = rptrekap.DataDefinition.ParameterFields
+            akhirPFD = akhirPFDs.Item("tglakhir") 'tanggal merupakan nama parameter
+            akhirPVs.Clear()
+            akhirPVs.Add(akhirPDV)
+            akhirPFD.ApplyCurrentValues(akhirPVs)
+
+            flappenjualan.CrystalReportViewer1.ReportSource = rptrekap
+            flappenjualan.ShowDialog()
+            flappenjualan.WindowState = FormWindowState.Maximized
+        Else
+            MsgBox("Data pada tanggal tersebut tidak tersedia", MsgBoxStyle.Information, "Pemberitahuan")
+        End If
+    End Sub
 End Class
