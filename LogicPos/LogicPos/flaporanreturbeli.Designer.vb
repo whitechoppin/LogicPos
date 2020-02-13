337e33be6ae6548898462ddfe33eeaa3bb0f76a1
Wendy Thung <wendy.thung93@gmail.com>
2020-02-13 12:30:11 +0800

    add - laporan retur beli

--- LogicPos/LogicPos/flaporanreturbeli.Designer.vb
+++ LogicPos/LogicPos/flaporanreturbeli.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class flaporanreturbeli
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,276 @@ Partial Class flaporanreturbeli
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.btnrekap = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.btnexcel = New System.Windows.Forms.Button()
+        Me.btntabel = New System.Windows.Forms.Button()
+        Me.btnperfaktur = New System.Windows.Forms.Button()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn6 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn7 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn8 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn9 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn10 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn11 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn12 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker2)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(278, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(780, 56)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Periode"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(360, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 16)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Sampai Dengan"
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Location = New System.Drawing.Point(489, 19)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(275, 24)
+        Me.DateTimePicker2.TabIndex = 3
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(76, 19)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(275, 24)
+        Me.DateTimePicker1.TabIndex = 2
+        '
+        'btnrekap
+        '
+        Me.btnrekap.Location = New System.Drawing.Point(210, 23)
+        Me.btnrekap.Name = "btnrekap"
+        Me.btnrekap.Size = New System.Drawing.Size(180, 50)
+        Me.btnrekap.TabIndex = 10
+        Me.btnrekap.Text = "Rekap Faktur"
+        Me.btnrekap.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.btnexcel)
+        Me.GroupBox4.Controls.Add(Me.btntabel)
+        Me.GroupBox4.Controls.Add(Me.btnperfaktur)
+        Me.GroupBox4.Controls.Add(Me.btnrekap)
+        Me.GroupBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox4.Location = New System.Drawing.Point(14, 74)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(1044, 83)
+        Me.GroupBox4.TabIndex = 15
+        Me.GroupBox4.TabStop = False
+        '
+        'btnexcel
+        '
+        Me.btnexcel.Location = New System.Drawing.Point(829, 23)
+        Me.btnexcel.Name = "btnexcel"
+        Me.btnexcel.Size = New System.Drawing.Size(200, 50)
+        Me.btnexcel.TabIndex = 17
+        Me.btnexcel.Text = "Convert Excel"
+        Me.btnexcel.UseVisualStyleBackColor = True
+        '
+        'btntabel
+        '
+        Me.btntabel.Location = New System.Drawing.Point(24, 23)
+        Me.btntabel.Name = "btntabel"
+        Me.btntabel.Size = New System.Drawing.Size(180, 50)
+        Me.btntabel.TabIndex = 17
+        Me.btntabel.Text = "Rekap Tabel"
+        Me.btntabel.UseVisualStyleBackColor = True
+        '
+        'btnperfaktur
+        '
+        Me.btnperfaktur.Location = New System.Drawing.Point(396, 23)
+        Me.btnperfaktur.Name = "btnperfaktur"
+        Me.btnperfaktur.Size = New System.Drawing.Size(180, 50)
+        Me.btnperfaktur.TabIndex = 10
+        Me.btnperfaktur.Text = "Rekap Detail Faktur"
+        Me.btnperfaktur.UseVisualStyleBackColor = True
+        '
+        'GridControl1
+        '
+        Me.GridControl1.Location = New System.Drawing.Point(14, 163)
+        Me.GridControl1.MainView = Me.GridView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Size = New System.Drawing.Size(1044, 486)
+        Me.GridControl1.TabIndex = 16
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'GridView1
+        '
+        Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn3, Me.GridColumn4, Me.GridColumn5, Me.GridColumn6, Me.GridColumn7, Me.GridColumn8, Me.GridColumn9, Me.GridColumn10, Me.GridColumn11, Me.GridColumn12})
+        Me.GridView1.GridControl = Me.GridControl1
+        Me.GridView1.Name = "GridView1"
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.OptionsColumn.AllowEdit = False
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 0
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Name = "GridColumn2"
+        Me.GridColumn2.OptionsColumn.AllowEdit = False
+        Me.GridColumn2.Visible = True
+        Me.GridColumn2.VisibleIndex = 1
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Name = "GridColumn3"
+        Me.GridColumn3.OptionsColumn.AllowEdit = False
+        Me.GridColumn3.Visible = True
+        Me.GridColumn3.VisibleIndex = 2
+        '
+        'GridColumn4
+        '
+        Me.GridColumn4.Name = "GridColumn4"
+        Me.GridColumn4.OptionsColumn.AllowEdit = False
+        Me.GridColumn4.Visible = True
+        Me.GridColumn4.VisibleIndex = 3
+        '
+        'GridColumn5
+        '
+        Me.GridColumn5.Name = "GridColumn5"
+        Me.GridColumn5.OptionsColumn.AllowEdit = False
+        Me.GridColumn5.Visible = True
+        Me.GridColumn5.VisibleIndex = 4
+        '
+        'GridColumn6
+        '
+        Me.GridColumn6.Name = "GridColumn6"
+        Me.GridColumn6.OptionsColumn.AllowEdit = False
+        Me.GridColumn6.Visible = True
+        Me.GridColumn6.VisibleIndex = 5
+        '
+        'GridColumn7
+        '
+        Me.GridColumn7.Name = "GridColumn7"
+        Me.GridColumn7.OptionsColumn.AllowEdit = False
+        Me.GridColumn7.Visible = True
+        Me.GridColumn7.VisibleIndex = 7
+        '
+        'GridColumn8
+        '
+        Me.GridColumn8.Name = "GridColumn8"
+        Me.GridColumn8.OptionsColumn.AllowEdit = False
+        Me.GridColumn8.Visible = True
+        Me.GridColumn8.VisibleIndex = 8
+        '
+        'GridColumn9
+        '
+        Me.GridColumn9.Name = "GridColumn9"
+        Me.GridColumn9.OptionsColumn.AllowEdit = False
+        Me.GridColumn9.Visible = True
+        Me.GridColumn9.VisibleIndex = 9
+        '
+        'GridColumn10
+        '
+        Me.GridColumn10.Name = "GridColumn10"
+        Me.GridColumn10.OptionsColumn.AllowEdit = False
+        Me.GridColumn10.Visible = True
+        Me.GridColumn10.VisibleIndex = 10
+        '
+        'GridColumn11
+        '
+        Me.GridColumn11.Name = "GridColumn11"
+        Me.GridColumn11.Visible = True
+        Me.GridColumn11.VisibleIndex = 6
+        '
+        'GridColumn12
+        '
+        Me.GridColumn12.Name = "GridColumn12"
+        Me.GridColumn12.Visible = True
+        Me.GridColumn12.VisibleIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(8, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(257, 31)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Laporan Retur Beli"
+        '
+        'flaporanreturbeli
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "flaporanreturbeli"
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1070, 661)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GridControl1)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "flaporanreturbeli"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Laporan Retur Pembelian"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents btnrekap As Button
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn6 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn7 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn8 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn9 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn10 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents btntabel As Button
+    Friend WithEvents GridColumn11 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents btnexcel As Button
+    Friend WithEvents GridColumn12 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnperfaktur As Button
 End Class
--- LogicPos/LogicPos/flaporanreturbeli.vb
+++ LogicPos/LogicPos/flaporanreturbeli.vb
@@ -1,3 +1,206 @@
-﻿Public Class flaporanreturbeli
+﻿Imports System.Data.Odbc
+Imports System.Globalization
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+Imports DevExpress.XtraGrid.Columns
+Public Class flaporanreturbeli
+    Public isi As String
+    Public isi2 As String
+    Private Sub flaporanpembelian_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.MdiParent = fmenu
+        Call koneksii()
 
+        DateTimePicker1.MaxDate = Now
+        DateTimePicker2.MaxDate = Now
+        Call grid()
+
+        With GridView1
+            .OptionsView.ShowFooter = True 'agar muncul footer untuk sum/avg/count
+            'buat sum harga
+            .Columns("subtotal").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "subtotal", "{0:n0}")
+            ' .Columns("keuntungan").Summary.Add(DevExpress.Data.SummaryItemType.Sum, "keuntungan", "{0:n0}")
+
+        End With
+    End Sub
+    Sub grid()
+        GridColumn1.Caption = "No.Retur"
+        GridColumn1.FieldName = "kode_retur"
+
+        GridColumn2.Caption = "Supplier"
+        GridColumn2.FieldName = "nama_supplier"
+        GridColumn2.Visible = False
+
+        GridColumn3.Caption = "Tanggal Retur Pembelian"
+        GridColumn3.FieldName = "tgl_returbeli"
+        GridColumn3.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn3.DisplayFormat.FormatString = "dd/MM/yyy"
+
+        GridColumn4.Caption = "Item"
+        GridColumn4.FieldName = "nama_barang"
+
+        GridColumn5.Caption = "Banyak"
+        GridColumn5.FieldName = "qty"
+
+        GridColumn6.Caption = "Satuan"
+        GridColumn6.FieldName = "satuan_barang"
+
+        GridColumn7.Caption = "Harga Beli"
+        GridColumn7.FieldName = "harga_beli"
+        GridColumn7.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn7.DisplayFormat.FormatString = "##,##0"
+
+        GridColumn8.Caption = "Subtotal"
+        GridColumn8.FieldName = "subtotal"
+        GridColumn8.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        GridColumn8.DisplayFormat.FormatString = "##,##0"
+
+        GridColumn9.Caption = "Kasir Penerima"
+        GridColumn9.FieldName = "kode_user"
+
+        GridColumn10.Visible = False
+        GridColumn11.Visible = False
+        GridColumn12.Caption = "Metode Bayar"
+        GridColumn12.FieldName = "pembayaran_pembelian"
+        GridColumn12.Visible = False
+
+        GridControl1.Visible = True
+    End Sub
+    Sub tabel()
+        Call koneksii()
+        Using cnn As New OdbcConnection(strConn)
+            If DateTimePicker1.Value.Equals(DateTimePicker2.Value) Then
+                sql = "SELECT * FROM tb_retur_pembelian_detail JOIN tb_retur_pembelian ON tb_retur_pembelian.kode_retur=tb_retur_pembelian_detail.kode_retur  WHERE DATE(tgl_returbeli) = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "'"
+            Else
+                sql = "SELECT * FROM tb_retur_pembelian_detail JOIN tb_retur_pembelian ON tb_retur_pembelian.kode_retur=tb_retur_pembelian_detail.kode_retur  WHERE tgl_returbeli BETWEEN '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "' AND '" & Format(DateTimePicker2.Value, "yyyy-MM-dd") & "'"
+            End If
+            da = New OdbcDataAdapter(sql, cnn)
+            ds = New DataSet
+            da.Fill(ds)
+            GridControl1.DataSource = Nothing
+            GridControl1.DataSource = ds.Tables(0)
+            Call grid()
+        End Using
+    End Sub
+
+    Private Sub btnrekap_Click(sender As Object, e As EventArgs) Handles btnrekap.Click
+        Dim rptrekap As ReportDocument
+        Dim awalPFDs As ParameterFieldDefinitions
+        Dim awalPFD As ParameterFieldDefinition
+        Dim awalPVs As New ParameterValues
+        Dim awalPDV As New ParameterDiscreteValue
+
+        Dim akhirPFDs As ParameterFieldDefinitions
+        Dim akhirPFD As ParameterFieldDefinition
+        Dim akhirPVs As New ParameterValues
+        Dim akhirPDV As New ParameterDiscreteValue
+
+        If DateTimePicker1.Value.Equals(DateTimePicker2.Value) Then
+            sql = "SELECT * FROM tb_retur_pembelian WHERE DATE(tgl_returbeli) = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "'"
+        Else
+            sql = "SELECT * FROM tb_retur_pembelian WHERE tgl_returbeli BETWEEN '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "' AND '" & Format(DateTimePicker2.Value, "yyyy-MM-dd") & "'"
+        End If
+
+        cmmd = New OdbcCommand(sql, cnn)
+        dr = cmmd.ExecuteReader
+
+        If dr.HasRows Then
+            rptrekap = New rptrekapreturpembelian
+
+            awalPDV.Value = Format(DateTimePicker1.Value, "yyyy-MM-dd")
+            awalPFDs = rptrekap.DataDefinition.ParameterFields
+            awalPFD = awalPFDs.Item("tglawal") 'tanggal merupakan nama parameter
+            awalPVs.Clear()
+            awalPVs.Add(awalPDV)
+            awalPFD.ApplyCurrentValues(awalPVs)
+
+            akhirPDV.Value = Format(DateTimePicker2.Value, "yyyy-MM-dd")
+            akhirPFDs = rptrekap.DataDefinition.ParameterFields
+            akhirPFD = akhirPFDs.Item("tglakhir") 'tanggal merupakan nama parameter
+            akhirPVs.Clear()
+            akhirPVs.Add(akhirPDV)
+            akhirPFD.ApplyCurrentValues(akhirPVs)
+
+            flappembelian.CrystalReportViewer1.ReportSource = rptrekap
+            flappembelian.ShowDialog()
+            flappembelian.WindowState = FormWindowState.Maximized
+        Else
+            MsgBox("Data pada tanggal tersebut tidak tersedia", MsgBoxStyle.Information, "Pemberitahuan")
+        End If
+    End Sub
+
+    Private Sub btntabel_Click(sender As Object, e As EventArgs) Handles btntabel.Click
+        Call tabel()
+    End Sub
+    Sub ExportToExcel()
+
+        Dim filename As String = InputBox("Nama File", "Input Nama file ")
+        Dim pathdata As String = "C:\ExportLogicPos"
+        Dim yourpath As String = "C:\ExportLogicPos\" + filename + ".xls"
+
+        If filename <> "" Then
+            If (Not System.IO.Directory.Exists(pathdata)) Then
+                System.IO.Directory.CreateDirectory(pathdata)
+            End If
+
+            GridView1.ExportToXls(yourpath)
+            MsgBox("Data tersimpan di " + yourpath, MsgBoxStyle.Information, "Success")
+            ' Do something
+        ElseIf DialogResult.Cancel Then
+            MsgBox("You've canceled")
+        End If
+    End Sub
+    Private Sub btnexcel_Click(sender As Object, e As EventArgs) Handles btnexcel.Click
+
+        If GridView1.DataRowCount > 0 Then
+            ExportToExcel()
+        Else
+            MsgBox("Export Gagal, Rekap Tabel terlebih dahulu  !", MsgBoxStyle.Information, "Gagal")
+        End If
+
+    End Sub
+
+    Private Sub btnperfaktur_Click(sender As Object, e As EventArgs) Handles btnperfaktur.Click
+        Dim rptrekap As ReportDocument
+        Dim awalPFDs As ParameterFieldDefinitions
+        Dim awalPFD As ParameterFieldDefinition
+        Dim awalPVs As New ParameterValues
+        Dim awalPDV As New ParameterDiscreteValue
+
+        Dim akhirPFDs As ParameterFieldDefinitions
+        Dim akhirPFD As ParameterFieldDefinition
+        Dim akhirPVs As New ParameterValues
+        Dim akhirPDV As New ParameterDiscreteValue
+
+        If DateTimePicker1.Value.Equals(DateTimePicker2.Value) Then
+            sql = "SELECT * FROM tb_retur_pembelian WHERE DATE(tgl_returbeli) = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "'"
+        Else
+            sql = "SELECT * FROM tb_retur_pembelian WHERE tgl_returbeli BETWEEN '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "' AND '" & Format(DateTimePicker2.Value, "yyyy-MM-dd") & "'"
+        End If
+        cmmd = New OdbcCommand(sql, cnn)
+        dr = cmmd.ExecuteReader
+
+        If dr.HasRows Then
+            rptrekap = New rptperfakturreturbeli
+
+            awalPDV.Value = Format(DateTimePicker1.Value, "yyyy-MM-dd")
+            awalPFDs = rptrekap.DataDefinition.ParameterFields
+            awalPFD = awalPFDs.Item("tglawal") 'tanggal merupakan nama parameter
+            awalPVs.Clear()
+            awalPVs.Add(awalPDV)
+            awalPFD.ApplyCurrentValues(awalPVs)
+
+            akhirPDV.Value = Format(DateTimePicker2.Value, "yyyy-MM-dd")
+            akhirPFDs = rptrekap.DataDefinition.ParameterFields
+            akhirPFD = akhirPFDs.Item("tglakhir") 'tanggal merupakan nama parameter
+            akhirPVs.Clear()
+            akhirPVs.Add(akhirPDV)
+            akhirPFD.ApplyCurrentValues(akhirPVs)
+
+            flappembelian.CrystalReportViewer1.ReportSource = rptrekap
+            flappembelian.ShowDialog()
+            flappembelian.WindowState = FormWindowState.Maximized
+        Else
+            MsgBox("Data pada tanggal tersebut tidak tersedia", MsgBoxStyle.Information, "Pemberitahuan")
+        End If
+    End Sub
 End Class
