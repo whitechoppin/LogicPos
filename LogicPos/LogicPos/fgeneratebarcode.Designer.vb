f145ab13ea0c607a674e04354ae0914a4db54b00
Alex Pratama Wibowo <alexanderwibowo.aw@gmail.com>
2020-07-15 16:25:27 +0800

    update barcode baru

--- LogicPos/LogicPos/fgeneratebarcode.Designer.vb
+++ LogicPos/LogicPos/fgeneratebarcode.Designer.vb
@@ -35,48 +35,48 @@ Partial Class fgeneratebarcode
         '
         'btnSave
         '
-        Me.btnSave.Location = New System.Drawing.Point(472, 237)
-        Me.btnSave.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnSave.Location = New System.Drawing.Point(306, 352)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(90, 25)
+        Me.btnSave.Size = New System.Drawing.Size(90, 32)
         Me.btnSave.TabIndex = 18
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = True
         '
         'txtInput
         '
-        Me.txtInput.Location = New System.Drawing.Point(13, 205)
-        Me.txtInput.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtInput.Location = New System.Drawing.Point(168, 320)
+        Me.txtInput.Margin = New System.Windows.Forms.Padding(4)
         Me.txtInput.Name = "txtInput"
-        Me.txtInput.Size = New System.Drawing.Size(549, 24)
+        Me.txtInput.Size = New System.Drawing.Size(228, 24)
         Me.txtInput.TabIndex = 17
         '
         'btnPrint
         '
-        Me.btnPrint.Location = New System.Drawing.Point(374, 237)
-        Me.btnPrint.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnPrint.Location = New System.Drawing.Point(208, 352)
+        Me.btnPrint.Margin = New System.Windows.Forms.Padding(4)
         Me.btnPrint.Name = "btnPrint"
-        Me.btnPrint.Size = New System.Drawing.Size(90, 25)
+        Me.btnPrint.Size = New System.Drawing.Size(90, 32)
         Me.btnPrint.TabIndex = 16
         Me.btnPrint.Text = "Print"
         Me.btnPrint.UseVisualStyleBackColor = True
         '
         'btnClear
         '
-        Me.btnClear.Location = New System.Drawing.Point(276, 237)
-        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnClear.Location = New System.Drawing.Point(110, 351)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(90, 25)
+        Me.btnClear.Size = New System.Drawing.Size(90, 32)
         Me.btnClear.TabIndex = 15
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
         '
         'btnGenerate
         '
-        Me.btnGenerate.Location = New System.Drawing.Point(178, 237)
-        Me.btnGenerate.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnGenerate.Location = New System.Drawing.Point(12, 351)
+        Me.btnGenerate.Margin = New System.Windows.Forms.Padding(4)
         Me.btnGenerate.Name = "btnGenerate"
-        Me.btnGenerate.Size = New System.Drawing.Size(90, 25)
+        Me.btnGenerate.Size = New System.Drawing.Size(90, 32)
         Me.btnGenerate.TabIndex = 14
         Me.btnGenerate.Text = "Generate"
         Me.btnGenerate.UseVisualStyleBackColor = True
@@ -84,20 +84,22 @@ Partial Class fgeneratebarcode
         'picBarcode
         '
         Me.picBarcode.BackColor = System.Drawing.SystemColors.Control
-        Me.picBarcode.Location = New System.Drawing.Point(13, 13)
-        Me.picBarcode.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.picBarcode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.picBarcode.Location = New System.Drawing.Point(12, 13)
+        Me.picBarcode.Margin = New System.Windows.Forms.Padding(4)
         Me.picBarcode.Name = "picBarcode"
-        Me.picBarcode.Size = New System.Drawing.Size(549, 184)
+        Me.picBarcode.Size = New System.Drawing.Size(384, 290)
         Me.picBarcode.TabIndex = 13
         Me.picBarcode.TabStop = False
         '
         'cbBarcodeType
         '
+        Me.cbBarcodeType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbBarcodeType.FormattingEnabled = True
         Me.cbBarcodeType.Items.AddRange(New Object() {"Barcode", "QR Code"})
-        Me.cbBarcodeType.Location = New System.Drawing.Point(12, 236)
+        Me.cbBarcodeType.Location = New System.Drawing.Point(12, 318)
         Me.cbBarcodeType.Name = "cbBarcodeType"
-        Me.cbBarcodeType.Size = New System.Drawing.Size(159, 26)
+        Me.cbBarcodeType.Size = New System.Drawing.Size(149, 26)
         Me.cbBarcodeType.TabIndex = 19
         '
         'fgeneratebarcode
@@ -105,7 +107,7 @@ Partial Class fgeneratebarcode
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(575, 282)
+        Me.ClientSize = New System.Drawing.Size(405, 397)
         Me.Controls.Add(Me.cbBarcodeType)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.txtInput)
@@ -114,7 +116,7 @@ Partial Class fgeneratebarcode
         Me.Controls.Add(Me.btnGenerate)
         Me.Controls.Add(Me.picBarcode)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "fgeneratebarcode"
         Me.Text = "Generate Barcode"
         CType(Me.picBarcode, System.ComponentModel.ISupportInitialize).EndInit()
--- LogicPos/LogicPos/fgeneratebarcode.vb
+++ LogicPos/LogicPos/fgeneratebarcode.vb
@@ -1,36 +1,76 @@
 ﻿Imports ZXing
 
 Public Class fgeneratebarcode
+
+    Dim WithEvents printDoc As New Printing.PrintDocument()
+
     Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
         Dim writer As New BarcodeWriter
-        If cbBarcodeType.SelectedIndex = 0 Then
-            writer.Format = BarcodeFormat.CODE_128
-            picBarcode.Image = writer.Write(txtInput.Text)
-        ElseIf cbBarcodeType.SelectedIndex = 1 Then
-            writer.Format = BarcodeFormat.QR_CODE
-            picBarcode.Image = writer.Write(txtInput.Text)
+
+        picBarcode.Image = Nothing
+
+        If txtInput.TextLength > 0 Then
+            If cbBarcodeType.SelectedIndex = 0 Then
+                writer.Options.Height = 150
+                writer.Options.Width = 380
+                writer.Format = BarcodeFormat.CODE_128
+                picBarcode.Image = writer.Write(txtInput.Text)
+            ElseIf cbBarcodeType.SelectedIndex = 1 Then
+                writer.Options.Height = 290
+                writer.Options.Width = 380
+                writer.Format = BarcodeFormat.QR_CODE
+                picBarcode.Image = writer.Write(txtInput.Text)
+            Else
+                MsgBox("Pilih Jenis Barcode")
+            End If
         Else
-            MsgBox("Pilih Jenis Barcode")
+            MsgBox("Isi Text Barcode")
         End If
+
     End Sub
 
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
         picBarcode.Image = Nothing
     End Sub
 
-    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+    Private Sub PrintImage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles printDoc.PrintPage
+        e.Graphics.DrawImage(picBarcode.Image, e.MarginBounds.Left, e.MarginBounds.Top)
+    End Sub
 
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        printDoc.Print()
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-        sfd.ShowDialog()
 
-        If sfd.FileName > "" Then
-            picBarcode.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
-        End If
+        With sfd
+            .Title = "Save Image As"
+            .Filter = "Jpg, Jpeg Images|*.jpg;*.jpeg|PNG Image|*.png|BMP Image|*.bmp"
+            .AddExtension = True
+            .DefaultExt = ".jpg"
+            .FileName = "My Barcode.jpg"
+            .ValidateNames = True
+            '.OverwritePrompt = True
+            '.InitialDirectory = Dir()
+            '.RestoreDirectory = True
+
+            If .ShowDialog = DialogResult.OK Then
+                If .FilterIndex = 1 Then
+                    picBarcode.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                ElseIf .FilterIndex = 2 Then
+                    picBarcode.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png)
+                ElseIf .FilterIndex = 3 Then
+                    picBarcode.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
+                End If
+            Else
+                Return
+            End If
+
+        End With
     End Sub
 
     Private Sub fgeneratebarcode_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Me.MdiParent = fmenu
+        cbBarcodeType.SelectedIndex = 0
     End Sub
 End Class
