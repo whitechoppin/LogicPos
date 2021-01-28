9b29af9d8c66cac7bd597cc95d8d45a54d64f7d4
Alexander Wibowo <alexanderwibowo@hotmail.com>
2021-01-28 10:01:40 +0800

    update minor aplikasi logic pos

--- LogicPos/LogicPos/fungsi.vb
+++ LogicPos/LogicPos/fungsi.vb
@@ -41,4 +41,34 @@ Module fungsi
 
         Return sResult
     End Function
+
+    Function ValidAngka(ByVal e As System.Windows.Forms.KeyPressEventArgs) As Boolean
+        Dim strValid As String = "0123456789"
+        If Strings.InStr(strValid, e.KeyChar) = 0 And Not (e.KeyChar = Strings.Chr(Keys.Back)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Function ValidDesimal(ByVal e As System.Windows.Forms.KeyPressEventArgs) As Boolean
+        Dim strValid As String = "0123456789,."
+        If Strings.InStr(strValid, e.KeyChar) = 0 And Not (e.KeyChar = Strings.Chr(Keys.Back)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Function ValidAngkaHuruf(ByVal e As System.Windows.Forms.KeyPressEventArgs) As Boolean
+        Dim strValid As String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ0123456789"
+        If Strings.InStr(strValid, e.KeyChar) = 0 And Not (e.KeyChar = Strings.Chr(Keys.Back)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    'pastekan di keypress
+    'e.Handled = Not (Char.IsDigit(e.KeyChar) OR e.KeyChar=".")
 End Module
--- LogicPos/LogicPos/prosedure.vb
+++ LogicPos/LogicPos/prosedure.vb
@@ -1,3 +1,75 @@
 ﻿Module prosedure
+    Sub offform()
+        fmenu.MenuUtama.Items.Item(0).Visible = True
+        fmenu.MenuUtama.Items.Item(1).Visible = True
+        fmenu.MenuUtama.Items.Item(2).Visible = True
+        fmenu.MenuUtama.Items.Item(3).Visible = True
+        fmenu.MenuUtama.Items.Item(4).Visible = True
+        fmenu.MenuUtama.Items.Item(5).Visible = True
 
+        fmenu.MasterMenu.DropDownItems.Item(0).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(1).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(2).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(3).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(4).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(5).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(6).Visible = False
+        fmenu.MasterMenu.DropDownItems.Item(7).Visible = False
+
+        fmenu.TransaksiMenu.DropDownItems.Item(0).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(1).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(2).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(3).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(4).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(5).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(6).Visible = False
+        fmenu.TransaksiMenu.DropDownItems.Item(7).Visible = False
+
+        fmenu.AdministrasiMenu.DropDownItems.Item(0).Visible = False
+        fmenu.AdministrasiMenu.DropDownItems.Item(1).Visible = False
+        fmenu.AdministrasiMenu.DropDownItems.Item(2).Visible = False
+        fmenu.AdministrasiMenu.DropDownItems.Item(3).Visible = False
+        fmenu.AdministrasiMenu.DropDownItems.Item(4).Visible = False
+
+        fmenu.LaporanMenu.DropDownItems.Item(0).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(1).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(2).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(3).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(4).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(5).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(6).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(7).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(8).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(9).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(10).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(11).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(12).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(13).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(14).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(15).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(16).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(17).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(18).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(19).Visible = False
+        fmenu.LaporanMenu.DropDownItems.Item(20).Visible = False
+
+        fmenu.ChartMenu.DropDownItems.Item(0).Visible = False
+        fmenu.ChartMenu.DropDownItems.Item(1).Visible = False
+        fmenu.ChartMenu.DropDownItems.Item(2).Visible = False
+        fmenu.ChartMenu.DropDownItems.Item(3).Visible = False
+        fmenu.ChartMenu.DropDownItems.Item(4).Visible = False
+        fmenu.ChartMenu.DropDownItems.Item(5).Visible = False
+
+        fmenu.FeatureMenu.DropDownItems.Item(0).Visible = False
+        fmenu.FeatureMenu.DropDownItems.Item(1).Visible = False
+
+        fmenu.SettingMenu.DropDownItems.Item(0).Visible = False
+        fmenu.SettingMenu.DropDownItems.Item(1).Visible = False
+        fmenu.SettingMenu.DropDownItems.Item(2).Visible = True
+        fmenu.SettingMenu.DropDownItems.Item(3).Visible = True
+        fmenu.SettingMenu.DropDownItems.Item(4).Visible = True
+        fmenu.SettingMenu.DropDownItems.Item(5).Visible = True
+        fmenu.SettingMenu.DropDownItems.Item(6).Visible = False
+        fmenu.SettingMenu.DropDownItems.Item(7).Visible = False
+    End Sub
 End Module
